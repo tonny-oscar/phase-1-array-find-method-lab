1db6194a346e65bed759f921ded954f50a6fd5ab
Tonny Bett <betttonny26@gmail.com>
2024-07-21 00:26:23 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function superbowlWin(record) {
+    const winningRecord = record.find(entry => entry.result === 'W');
+    return winningRecord ? winningRecord.year : undefined;
+}
+
+const superBowlRecords = [
+    { year: "2016", team: "Denver Broncos", result: "W" },
+    { year: "2015", team: "Denver Broncos", result: "W" },
+    { year: "2013", team: "Denver Broncos", result: "L" },
+];
+
+console.log(superbowlWin(superBowlRecords)); 
